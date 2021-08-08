5f216dd2fda9265f9cbab87606c85275effd173c
Andy James <andrewj@ksharp.net>
2021-08-08 19:27:59 +0100

    Initial aether venues app

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import { Venue } from "./components/Venue";
+import venues from "./venues.json";
+
+function App() {
+
+  let venueViewModels = [];
+
+  for (const venue of venues) {
+    for (const time of venue.times) {
+      const {times, ...strippedVenue} = venue;
+      venueViewModels.push({ 
+        ...strippedVenue,
+        day: time.day,
+        start: time.start,
+        end: time.end 
+      });
+    }
+  }
+
+  venueViewModels = venueViewModels.sort((a, b) => a.day - b.day)
+
+  return (
+    <div className="app">
+      <h1>Aether Venues</h1>
+      <div className="venues">
+        { venueViewModels.map(v => <Venue {...v} /> )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
