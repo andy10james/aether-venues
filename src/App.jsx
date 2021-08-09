390842294bbd340effc12c89c7514473142ceb73
Andy James <andrewj@ksharp.net>
2021-08-09 01:20:15 +0100

    Add definition for venue, and AV icon

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,18 @@ function App() {
   venueViewModels = venueViewModels.sort((a, b) => a.day - b.day)
 
   return (
-    <div className="app">
-      <h1>Aether Venues</h1>
-      <div className="venues">
+    <div className="aether-venues">
+      <div className="aether-venues__heading">
+        <img src="logo.png" alt="" />
+        <h1>Aether Venues</h1>
+      </div>
+      <p className="aether-venues__definition">
+      A venue is a place maintained by players for all RPers to come by and RP in. This includes taverns, shops, nightclubs, restaurants or even crazier environments like colosseums. They often include various RP "services" such as in-house photography, artists, tarrot readings and courtesans. Venues must have a regularly scheduled open time to be on this list. 
+      </p>
+      <div className="aether-venues__venues">
         { venueViewModels.map(v => <Venue {...v} /> )}
       </div>
-      <div className="made-by">Made with <span>♥</span> by <a target="_blank" rel="noreferrer" href="https://discordapp.com/users/236852510688542720">Kana Ki</a>, Gilgamesh.</div>
+      <div className="aether-venues__made-by">Made with <span>♥</span> by <a target="_blank" rel="noreferrer" href="https://discordapp.com/users/236852510688542720">Kana Ki</a>, Gilgamesh.</div>
     </div>
   );
 }
