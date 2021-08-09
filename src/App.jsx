4e03efdc25e0082ab97ac7dd8b5046a4d445dbe2
Andy James <andrewj@ksharp.net>
2021-08-10 00:16:48 +0100

    Improve design of aethervenues

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
 import './App.css';
-import { Venue } from "./components/Venue";
+import { Venue } from "./components/venue/Venue";
 import venues from "./venues.json";
+import days from "./consts/days.json";
 
-function App() {
+export default function App() {
 
-  let venueViewModels = [];
-
-  for (const venue of venues) {
-    for (const time of venue.times) {
-      const {times, ...strippedVenue} = venue;
-      venueViewModels.push({ 
-        ...strippedVenue,
-        day: time.day,
-        start: time.start,
-        end: time.end 
-      });
-    }
-  }
-
-  venueViewModels = venueViewModels.sort((a, b) =>  {
-    const dayDiff = a.day - b.day;
-    if (dayDiff !== 0) {
-      return dayDiff;
-    }
-    let aStartTime = parseInt(a.start);
-    if (aStartTime < 600) aStartTime += 2400;
-    let bStartTime = parseInt(b.start);
-    if (bStartTime < 600) bStartTime += 2400;
-    return aStartTime - bStartTime;
-  })
-
+  const venueViewModels = getVenueViewModels();
+  
   return (
     <div className="aether-venues">
       <div className="aether-venues__heading">
@@ -37,14 +14,47 @@ function App() {
         <h1>Aether Venues</h1>
       </div>
       <p className="aether-venues__definition">
-      A venue is a place maintained by players for all RPers to come by and RP in. This includes taverns, shops, nightclubs, restaurants or even crazier environments like colosseums. They often include various RP "services" such as in-house photography, artists, tarrot readings and courtesans. Venues must have a regularly scheduled opening time to be on this list. 
+        A venue is a place maintained by players for all RPers to come by and RP in. This includes taverns, shops, nightclubs, restaurants or even crazier environments like colosseums. They often include various RP "services" such as in-house photography, artists, tarrot readings and courtesans. Venues must have a regularly scheduled opening time to be on this list. 
       </p>
       <div className="aether-venues__venues">
-        { venueViewModels.map(v => <Venue {...v} /> )}
+        { venueViewModels.map((venues, i) =>
+            <div className="aether-venues__day" key={i}>
+              <details open>
+                <summary><h2>{days[i]}</h2></summary>
+                { venues.map((v, i) => <Venue {...v} key={i} /> )}
+              </details>
+            </div>
+        )}
       </div>
       <div className="aether-venues__made-by">Made with <span>♥</span> by <a target="_blank" rel="noreferrer" href="https://discordapp.com/users/236852510688542720">Kana Ki</a>, Gilgamesh.</div>
     </div>
   );
 }
 
-export default App;
+function getVenueViewModels() {
+  let venueViewModels = [[], [], [], [], [], [], []];
+
+  for (const venue of venues) {
+    for (const time of venue.times) {
+      const { times, ...strippedVenue } = venue;
+      const venueViewModel = {
+        ...strippedVenue,
+        time
+      };
+      venueViewModels[time.day].push(venueViewModel);
+    }
+  }
+
+  for (let day = 0; day <= 6; day++) {
+    venueViewModels[day] = venueViewModels[day].sort((a, b) => {
+      let aStartTime = parseInt(a.start);
+      if (aStartTime < 600)
+        aStartTime += 2400;
+      let bStartTime = parseInt(b.start);
+      if (bStartTime < 600)
+        bStartTime += 2400;
+      return aStartTime - bStartTime;
+    });
+  }
+  return venueViewModels;
+}
