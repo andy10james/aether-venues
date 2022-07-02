233882684e862aed4914092cb16845b1fd9f1977
Andy James <andrewj@ksharp.net>
2022-07-02 17:13:59 +0100

    Fix formatting for Venue Strip

--- src/components/venue-strip/VenueStrip.jsx
+++ src/components/venue-strip/VenueStrip.jsx
@@ -6,9 +6,9 @@ import { Loading } from "../loading/Loading";
 export function VenueStrip(props) {
     return <div className="venue-strip">
         <HorizontalScroll reverseScroll>
-        { props.venues === null || props.venues === undefined
-        ? [ ...Array(Math.floor(Math.random() * 3) + 1)].map((_, i) => <Loading key={i} />)
-        : props.venues.map((v) => <VenueCard venue={v.venue || v} opening={v.opening} key={(v.venue || v).id} /> )}
+            { props.venues === null || props.venues === undefined
+            ? [ ...Array(Math.floor(Math.random() * 3) + 1)].map((_, i) => <Loading key={i} />)
+            : props.venues.map((v) => <VenueCard venue={v.venue || v} opening={v.opening} key={(v.venue || v).id} /> )}
         </HorizontalScroll>
     </div>
 }
