1de495db849aebb59678134bb7e9c2f7922453d4
kana <kana@ffxivvenues.com>
2023-04-06 00:38:53 +0100

    Add DDOS notice

--- src/components/notice/notice.jsx
+++ src/components/notice/notice.jsx
@@ -1,36 +1,9 @@
-import React, { useState } from "react";
-import { Modal } from "../modal/Modal";
-import { Button } from "../button/Button";
-import banner from "./banner.png";
+import React from "react";
 import "./notice.css";
 
 function Notice() {
-    const [modalActive, setModalActive] = useState(false);
-
-    return (<div className='notice' onClick={_ => setModalActive(!modalActive)}>
-        <strong>Burgundy Gives Back</strong> is currently live! A 12-hour charity event for Trans Lifeline.
-        { modalActive &&
-            <Modal onStageClick={_ => setModalActive(false)}
-                   style={{
-                        textAlign: "center",
-                       width: 800,
-                       fontSize: 0
-                   }}>
-                <Button className="venue-modal__close-button"
-                        style={{ backgroundColor: "#181818", border: "none"}}
-                        onClick={_=> setModalActive(false)}>
-                    <img src="assets/cross.svg" alt="" />
-                </Button>
-                <a href="https://burgundygivesback.com/">
-                    <img alt="FFXIV Venues is now in Europe" src={banner} style={{
-                        borderTopLeftRadius: 5,
-                        borderTopRightRadius: 5,
-                        width: "100%",
-                        margin: 0
-                    }}/>
-                </a>
-            </Modal>
-        }
+    return (<div className='notice'>
+        <strong>Card View is currently disabled</strong> while we work to mitigate an attack. Send us love please 😢.
     </div>)
 }
 
