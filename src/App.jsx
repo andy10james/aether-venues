53a5592a129b9697c0c78f4ba7116d78b17b2323
kana <kana@ffxivvenues.com>
2023-04-20 01:17:34 +0100

    Restore banners

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import { PersonList } from './components/person-list/PersonList';
 import { staff, patrons} from "./components/person-list/PeopleLists";
 import { ReactComponent as DiscordIcon } from "./assets/icons/discord-icon.svg";
 import { ReactComponent as NewVenue } from "./assets/icons/new-venue-icon.svg";
-// import { ReactComponent as ListViewIcon } from "./assets/icons/list-view-icon.svg";
-// import { ReactComponent as CardViewIcon } from "./assets/icons/card-view-icon.svg";
+import { ReactComponent as ListViewIcon } from "./assets/icons/list-view-icon.svg";
+import { ReactComponent as CardViewIcon } from "./assets/icons/card-view-icon.svg";
 import { NewVenueGuidance } from './components/new-venue-guidance/NewVenueGuidance';
 import { Button } from './components/button/Button';
 
@@ -49,27 +49,17 @@ class App extends React.Component {
         <div className="aether-venues">
           <div className="aether-venues__heading">
             <h1><img src="full-logo.png" alt="FFXIV Venues" /></h1>
-
-            {/*<div className="aether-venues__view-toggle">*/}
-            {/*  <button onClick={this.switchToListView} className={this.state.listView ? `active` : undefined}><ListViewIcon /> List view</button>*/}
-            {/*  <button onClick={this.switchToCardView} className={this.state.listView ? undefined : `active`}><CardViewIcon /> Card view</button>*/}
-            {/*</div>*/}
+            <div className="aether-venues__view-toggle">
+              <button onClick={this.switchToListView} className={this.state.listView ? `active` : undefined}><ListViewIcon /> List view</button>
+              <button onClick={this.switchToCardView} className={this.state.listView ? undefined : `active`}><CardViewIcon /> Card view</button>
+            </div>
             <div className="aether-venues__colaborators">
               In collaboration with
               <a href="https://aetherentertainer.carrd.co/" target="_blank" rel="noreferrer"><img src="aether-entertainer.png" alt="Aether Entertainer Gazette" /></a>
             </div>
           </div>
           <div className={`aether-venues__list ${ this.state.listView ? `aether-venues__list--list-view` : `aether-venues__list--card-view` }`}>
-            <VenueDirectory listView={true} />
-            <span className="aether-venues__made-by">Made with ❤️ by Kana Ki.</span>
-            <span className="aether-venues__patreons">Supported by our Patrons ❤️
-              <img src={`https://img2.finalfantasyxiv.com/f/63cdf881f6443d20084d006afdba7b87_745baffc465480ed372e274d50318290fc0_96x96.jpg`} alt=""/> 
-              <a target="_blank" rel="noreferrer" href="https://na.finalfantasyxiv.com/lodestone/character/38338653/">Autumn Dream</a> (Excalibur),
-              <img src={`https://img2.finalfantasyxiv.com/f/28b7a4b12553495d9217cae3bfa68acb_8106f857613f8fb994b0be37b26ff4bafc0_96x96.jpg`} alt=""/>
-              <a target="_blank" rel="noreferrer" href="https://eu.finalfantasyxiv.com/lodestone/character/42596715/">Ophelia Stormslayer</a> (Zalera), and
-              <img src={`https://img2.finalfantasyxiv.com/f/5f070152baf12ac5d75ce1d657bb1d86_5c8ecfbc673e1287a9b5e85423fe1657fc0_96x96.jpg`} alt=""/>
-              <a target="_blank" rel="noreferrer" href="https://na.finalfantasyxiv.com/lodestone/character/33428822/">Legiana Skywrath</a> (Midgardsormr).
-            </span>
+            <VenueDirectory listView={this.state.listView} />
           </div>
           <div className="aether-venues__meta-panel">
             <div className="aether-venues__discord-panels">
