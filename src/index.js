251d74d56755f61d326d10a6457fc7cdce6ea942
kana <kana@ffxivvenues.com>
2023-07-23 17:34:07 +0100

    Add new hero images

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
+document.body.style.backgroundImage = [
+    `url("hero-strip/hero-strip-0.jpg")`,
+    `url("hero-strip/hero-strip-1.jpg")`,
+    `url("hero-strip/hero-strip-2.jpg")`
+][Math.round(Math.random()*2)];
+
 const root = createRoot(document.querySelector('.app-root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
