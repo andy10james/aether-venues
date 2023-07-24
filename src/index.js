23cb8ae4ec8dba20572811cb3b2f3b9b8ba9f235
Kana Ki <kana@ksharp.net>
2023-07-24 03:06:32 +0100

    Change how random banner is applied so to fix mobile issue

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
-document.body.style.backgroundImage = [
+document.querySelector(':root').style.setProperty('--hero-image', [
   `url("hero-strip/hero-strip-0.jpg")`,
   `url("hero-strip/hero-strip-1.jpg")`,
   `url("hero-strip/hero-strip-2.jpg")`
-][Math.round(Math.random()*2)];
+][Math.round(Math.random()*2)]);
 
 const root = createRoot(document.querySelector('.app-root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
