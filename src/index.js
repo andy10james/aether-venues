6bbf82c7b97550ee001a7ea5e929823719e19003
Kana Ki <kana@ksharp.net>
2023-07-27 03:21:10 +0100

    Attempt to fix banner strip not showing

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { App } from './App';
 import './index.css';
 
 document.querySelector(':root').style.setProperty('--hero-image', [
-  `url("${process.env.PUBLIC_URL}hero-strip/hero-strip-0.jpg")`,
-  `url("${process.env.PUBLIC_URL}hero-strip/hero-strip-1.jpg")`,
-  `url("${process.env.PUBLIC_URL}hero-strip/hero-strip-2.jpg")`
+  `url("/hero-strip/hero-strip-0.jpg")`,
+  `url("/hero-strip/hero-strip-1.jpg")`,
+  `url("/hero-strip/hero-strip-2.jpg")`
 ][Math.floor(Math.random()*3)]);
 const root = createRoot(document.querySelector('.app-root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
