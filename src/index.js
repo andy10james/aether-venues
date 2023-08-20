56a2b4cc1026d6c48882bdd49e757b6b50ef5cb1
Kana Ki <kana@ksharp.net>
2023-08-20 03:11:21 +0100

    Add Vix to banner rotation

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import './index.css';
 document.querySelector(':root').style.setProperty('--hero-image', [
   `url("/hero-strip/hero-strip-0.jpg")`,
   `url("/hero-strip/hero-strip-1.jpg")`,
-  `url("/hero-strip/hero-strip-2.jpg")`
-][Math.floor(Math.random()*3)]);
+  `url("/hero-strip/hero-strip-2.jpg")`,
+  `url("/hero-strip/hero-strip-3.jpg")`
+][Math.floor(Math.random()*4)]);
 const root = createRoot(document.querySelector('.app-root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
