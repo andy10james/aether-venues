7be976a780a264972118595ccb31e08588a8fad3
kana <kana@ffxivvenues.com>
2023-08-20 17:24:06 +0100

    Remove form from new venue modal

--- src/components/new-venue-guidance/NewVenueGuidance.jsx
+++ src/components/new-venue-guidance/NewVenueGuidance.jsx
@@ -24,35 +24,10 @@ export function NewVenueGuidance()  {
                     </li>
                 </ol>
             </div>
-            <div className="new-venue-guidance__spine">
-                <div className="new-venue-guidance__spine-line" />
-                <div className="new-venue-guidance__spine-text">OR</div>
-                <div className="new-venue-guidance__spine-line" />
-            </div>
-            <div className="new-venue-guidance__guidance new-venue-guidance__form-guidance">
-                <h2>Join via an Indexer</h2>
-                <p>Need that <s>human</s> Miqo'te touch? Complete the form below and within a few days we'll add you to the index personally.</p>
-                <ol>
-                    <li>
-                        <div className="new-venue-guidance__join-discord-step">
-                            Tell us about your venue via a short form!
-                            <br/>
-                            <Button className="new-venue-guidance__button" href="https://form.jotform.com/220493425516050">Complete the form!</Button>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="new-venue-guidance__join-discord-step">
-                            Join the home of FFXIV Venues!
-                            <br/>
-                            <Button className="new-venue-guidance__button" href="https://discord.gg/gTP65VYcMj">Join the discord!</Button>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="new-venue-guidance__join-discord-step">
-                            Wait to hear back from an Indexer ♥
-                        </div>
-                    </li>
-                </ol>
+            <div className="new-venue-guidance__spine"></div>
+            <div className="new-venue-guidance__guidance new-venue-guidance__discord-guidance">
+                <p>It's important to stay in the FFXIV Venues discord server so Veni can reach you and maintain your venue on the site. Free free to mute the server if you prefer. 🥰</p>
+                <p>If you have any questions or need help, just make a Venue ticket in the discord or send a direct message to <a href="https://discordapp.com/users/236852510688542720">Kana Ki</a>.</p>
             </div>
         </div>
     </>
