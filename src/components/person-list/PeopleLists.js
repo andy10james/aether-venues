cc1b15220549e0f73698b7132c3cbeb14e115a8e
kana <kana@ffxivvenues.com>
2023-10-10 03:25:59 +0100

    Add new CE/Photographer and Event Planner

--- src/components/person-list/PeopleLists.js
+++ src/components/person-list/PeopleLists.js
@@ -3,14 +3,21 @@ export const staff = [
     { name: "Sumi Satsuo", world: "Jenova", role: "Engineer", photoId: "6adbef94cc3fa361f6a047330a0b9a44_ce736afe35e2ded4e46c4fd0659aef7efc0" , discordId: "252142384303833088" },
     { name: "Madame Akehime", world: "Jenova", role: "React Engineer", photoId: "117ac0f2238aac7e46c0284dc6ffeaa7_ce736afe35e2ded4e46c4fd0659aef7efc0" , discordId: "159859092603535360" },
     { name: "Klyhia Moni", world: "Moogle", role: ".NET Engineer", photoId: "caf8839602db852c205fff52c76e5921_d7a9d5f85a29d6278ec1c7adc2c8d242fc0" , discordId: "133149295732850688" },
+
     { name: "Josean Xai", world: "Marilith", role: "Photography Lead", photoId: "d6583919ef6756c46ee9cac82110041a_284358f8eb4efc9095914e46798c6ab3fc0", link: "https://josean.xai.ffxivphotography.com/#sfwo" },
-    { name: "Vix Solanine", world: "Leviathan", role: "Photographer (NA) & Jnr Moderator", photoId: "7ebe3d3ddadcd9ef4f5bb8358b5b6028_a91aae52cff9ef65932db06b150ffd47fc0" , link: "https://vixsolanine.carrd.co/" },
+    { name: "Vix Solanine", world: "Leviathan", role: "Photographer (NA) & Jnr Mod", photoId: "7ebe3d3ddadcd9ef4f5bb8358b5b6028_a91aae52cff9ef65932db06b150ffd47fc0" , link: "https://vixsolanine.carrd.co/" },
     { name: "Amatsu Kuwashima", world: "Phoenix", role: "Photographer (EU)", photoId: "b62177d1065ce573cb3eb75ee6e928d0_5047bc596a4bab2dc7f7c120bb22dec5fc0", link: "https://amatsupng.crd.co/#" },
+    { name: "Juno V'mhari", world: "Sophia", role: "Photographer (OCE)", photoId: "66f3e42541072d69e5dc4672c49769d9_ba15a18cd3a8fb567e39053022515eb6fc0", discordId: "152015224281628672" },
+
     { name: "Kaeda Destrian", world: "Jenova", role: "CE Lead", photoId: "325546268785ccad39ac94c00b0e044a_ce736afe35e2ded4e46c4fd0659aef7efc0" , discordId: "870413151676551178" },
     { name: "Kestra Alsta", world: "Louisoix", role: "CE (EU)", photoId: "706f605b80cf4ff5cd858a7a60bda6da_0a122616f3718d3f45cab10fd8fc1604fc0", discordId: "811738117761400853" },
+    { name: "Dalnariel Doh", world: "Sophia", role: "CE (OCE)", photoId: "bac6010da3d2ef72b6e2eb845264e7fd_ba15a18cd3a8fb567e39053022515eb6fc0", discordId: "176021916354740224" },
+
     { name: "Monsieur Zahmo'wo", world: "Jenova", role: "Event Lead & CE (NA)", photoId: "4f7246a71bea833241b0a30b88a78320_ce736afe35e2ded4e46c4fd0659aef7efc0", discordId: "786407181804896257" },
     { name: "Onika Kneiz", world: "Ultros", role: "Event Lead", photoId: "937aa1134de8ee8584f2df592d0a7a45_c514cdcdb619439df97d906d4434ccc6fc0", discordId: "160844140022726657" },
     { name: "Whisper Blackvell", world: "Malboro", role: "Event Planner", photoId: "e97f726e42a71f690c871fe17df25cd7_2e97c13fdd593d15d543093f8a37b6f0fc0", discordId: "1061075402728165446" },
+    { name: "G'raha Tia''", world: "Sargatanas", role: "Event Planner", photoId: "ef9fc27410b6c5afb9fbb8b8444b4442_ba22853447012a24cee115315d6a5bebfc0", discordId: "194621026871541760" },
+
     { name: "Lanna'baker Kha", world: "Cactuar", role: "Moderation Lead", photoId: "8eb07e14239fb47e9535cf6f0bfdc9ba_96ab1df8877c1f8ba6a89a39cccfd437fc0" , discordId: "880594476295389205" },
     { name: "Uchu Jupiter", world: "Jenova", role: "Senior Moderator", photoId: "adb90390ac9bdd5a39a042dd5676c8c9_ce736afe35e2ded4e46c4fd0659aef7efc0" , discordId: "99616043571380224" },
     { name: "Alitzia Kiryu", world: "Siren", role: "Senior Moderator", photoId: "777dba0957855f16eba5fe0b902c0c37_58a84e851e55175d22158ca97af58a1ffc0" , discordId: "158410288238952449" },
