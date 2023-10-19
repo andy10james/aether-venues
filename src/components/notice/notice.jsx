ea669288fb8aa4f4be6dc5ce41a3634759c86ab0
Kana Ki <kana@ksharp.net>
2023-10-19 20:09:03 +0100

    Update notice for Echopalooza

--- src/components/notice/notice.jsx
+++ src/components/notice/notice.jsx
@@ -13,7 +13,7 @@ function Notice() {
 
     return <div className='notice' onClick={_ => setModalActive(true)}>
 
-        ♥ We're asking for your support. Event starts in 2 hours.
+        ♥ We're asking for your support. Event starts in 1 hours.
 
         { modalActive &&
         <Modal onStageClick={_ => setModalActive(false)} style={{ width: 550 }}>
