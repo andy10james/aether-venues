96abd679ac4ba8ee3b7ba938bc23447354269611
Sumioli <sumi_satsuo@protonmail.com>
2023-12-06 20:08:06 -0300

    added Lily and removed Klyhia

--- src/components/person-list/PeopleLists.js
+++ src/components/person-list/PeopleLists.js
@@ -2,7 +2,6 @@ export const staff = [
     { name: "Kana Ki", world: "Gilgamesh", role: "Engineering Lead", photoId: "5370f299860d4771c8454e6dd5057ddc_b937560c841465f7c4bc8eb47ea7948afc0" , discordId: "236852510688542720" },
     { name: "Sumi Satsuo", world: "Jenova", role: "Engineer", photoId: "6adbef94cc3fa361f6a047330a0b9a44_ce736afe35e2ded4e46c4fd0659aef7efc0" , discordId: "252142384303833088" },
     { name: "Madame Akehime", world: "Jenova", role: "React Engineer", photoId: "117ac0f2238aac7e46c0284dc6ffeaa7_ce736afe35e2ded4e46c4fd0659aef7efc0" , discordId: "159859092603535360" },
-    { name: "Klyhia Moni", world: "Moogle", role: ".NET Engineer", photoId: "caf8839602db852c205fff52c76e5921_d7a9d5f85a29d6278ec1c7adc2c8d242fc0" , discordId: "133149295732850688" },
 
     { name: "Josean Xai", world: "Marilith", role: "Photography Lead", photoId: "d6583919ef6756c46ee9cac82110041a_284358f8eb4efc9095914e46798c6ab3fc0", link: "https://josean.xai.ffxivphotography.com/#sfwo" },
     { name: "Vix Solanine", world: "Leviathan", role: "Photographer (NA) & Jnr Mod", photoId: "7ebe3d3ddadcd9ef4f5bb8358b5b6028_a91aae52cff9ef65932db06b150ffd47fc0" , link: "https://vixsolanine.carrd.co/" },
@@ -21,6 +20,8 @@ export const staff = [
     { name: "Lanna'baker Kha", world: "Cactuar", role: "Moderation Lead", photoId: "8eb07e14239fb47e9535cf6f0bfdc9ba_96ab1df8877c1f8ba6a89a39cccfd437fc0" , discordId: "880594476295389205" },
     { name: "Uchu Jupiter", world: "Jenova", role: "Senior Moderator", photoId: "adb90390ac9bdd5a39a042dd5676c8c9_ce736afe35e2ded4e46c4fd0659aef7efc0" , discordId: "99616043571380224" },
     { name: "Alitzia Kiryu", world: "Siren", role: "Senior Moderator", photoId: "777dba0957855f16eba5fe0b902c0c37_58a84e851e55175d22158ca97af58a1ffc0" , discordId: "158410288238952449" },
+    { name: "Lily Night", world: "Siren", role: "Moderator", photoId: "c36c1dbb999a3d33e518c6bd37539d40_58a84e851e55175d22158ca97af58a1ffc0" , discordId: "320380338524389376" },
+
 ];
 
 export const patrons = [
