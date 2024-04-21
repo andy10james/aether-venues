55b0ef737374fe0226b7ad156dff69d9e44d5b03
Kana Ki <kana@ksharp.net>
2024-04-21 04:18:48 +0100

    Add monthly schedules relative to end of the month

--- src/model/Schedule.js
+++ src/model/Schedule.js
@@ -1,3 +1,6 @@
+import days from "../consts/days.json";
+import {nth} from "../components/date-string/Nth";
+
 class Schedule {
 
     constructor(props) {
@@ -7,6 +10,44 @@ class Schedule {
             start: new Date(this.resolution.start),
             end: new Date(this.resolution.end)
         };
+    }
+
+    toString() {
+        let string = "";
+        let day = days[(this.resolution.start.getDay()+6)%7];
+        if (this.interval.intervalType === 0) {
+            if (this.interval.intervalArgument === 1)
+                string += "Weekly on ";
+            else if (this.interval.intervalArgument === 2)
+                string += "Biweekly on ";
+            else {
+                string += this.interval.intervalArgument;
+                string += " weekly on ";
+            }
+            string += day;
+            string += 's';
+        }
+        else if (this.interval.intervalType === 1 && this.interval.intervalArgument > 0) {
+            string += this.interval.intervalArgument;
+            string += nth(this.interval.intervalArgument);
+            string += ' ';
+            string += day;
+            string += " of the month";
+        }
+        else if (this.interval.intervalType === 1 && this.interval.intervalArgument === -1) {
+            string += "Last ";
+            string += day;
+            string += " of the month";
+        }
+        else if (this.interval.intervalType === 1 && this.interval.intervalArgument < -1) {
+            string += Math.abs(this.interval.intervalArgument);
+            string += nth(Math.abs(this.interval.intervalArgument));
+            string += " last ";
+            string += day;
+            string += " of the month";
+        }
+
+        return string;
     }
 
 }
